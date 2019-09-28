5f05cabe6ae8144126b87c4e8e6125e66b3c519d
Gincral <ssssrxdd@gmail.com>
2019-09-28 01:11:08 -0400

    cdowi

---
+++ Assets/interactWithPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactWithPlayer : MonoBehaviour
+{
+
+    private Collider2D collider;
+    // Start is called before the first frame update
+    void Start()
+    {   
+       //collider = getComp
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //this.
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.log()
+    }
+}   
+
