1a237d7b7e15be5b7e271517d62b5c0211226ebb
Gincral <ssssrxdd@gmail.com>
2019-09-28 15:45:13 -0400

    fneij

---
+++ Assets/Scripts/GravField.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravField : MonoBehaviour
+{
+    GameObject gravObject;
+    GameObject player;
+    public string PlayerTag = "Player";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+        
+        gravObject = gameObject.transform.Find("CenterOfGravity").gameObject;
+        player = GameObject.FindGameObjectWithTag(PlayerTag);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //var centerOfGrav = gameobject.transform.Find("CenterOfGravity");
+        Debug.Log("Entered", gravObject);
+        player.GetComponent<Player_Movement>().SetOwnCenterOfGravity(gravObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Entered");
+    }
+
+
+    void OnTrigger(Collider other)
+    {
+        Debug.Log("Entered");
+    }
+}
