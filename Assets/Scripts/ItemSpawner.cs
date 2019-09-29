4508c6701a74ef85ef5c304ed3c443514cb0affd
RunEMC <runlin.li@gmail.com>
2019-09-29 13:49:26 -0400

    Add item spawn script

---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject itemToSpawn;
+    public float radius;
+    public int timeBetweenSpawn = 500;
+    public int timeElapsed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (itemToSpawn == null) itemToSpawn = GameObject.Find("Item");
+        radius = gameObject.transform.lossyScale.z + 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeElapsed >= timeBetweenSpawn)
+        {
+            Vector2 pointInside = Random.insideUnitCircle.normalized * radius;
+            Vector3 randomPoint = new Vector3(pointInside.x, pointInside.y, 0) + gameObject.transform.position;
+            //Debug.Log(randomPoint.x);
+            //Debug.Log(randomPoint.z);
+            itemToSpawn.GetComponent<GravityTowardPoint>().SetOwnCenterOfGravity(gameObject.transform.Find("CenterOfGravity").gameObject, 1);
+            Instantiate(itemToSpawn, randomPoint, Quaternion.identity);
+            timeElapsed = 0;
+        }
+        else
+        {
+            timeElapsed++;
+        }
+        
+    }
+}
